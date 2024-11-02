eae8c044611a5e6a9f58420b4e00548110c2a317
SerhiiDziuba1309 <doktorschuuu@gmail.com>
2024-11-02 14:34:41 +0100

    add Feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,66 @@
-import React from "react";
-import Profile from "./Profile/Profile.jsx";
-import userData from "../userData.json";
-import FriendList from "./FriendList/FriendList.jsx";
-import friends from "../friends.json";
-import TransactionHistory from "./TransactionHistory/TransactionHistory.jsx";
-import transactions from "../transactions.json";
-import "./App.css";
-
+import React, { useState, useEffect } from 'react';
+import Feedback from './Feedback/Feedback.jsx';
+import Notification from './Notification/Notification.jsx';
+import Options from './Options/Options.jsx';
+import './App.css';
 const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  useEffect(() => {
+    const savedFeedback = JSON.parse(localStorage.getItem('feedback'));
+    if (savedFeedback) {
+      setFeedback(savedFeedback);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('feedback', JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = feedbackType => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
+
+  const resetFeedback = () => {
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positivePercentage =
+    totalFeedback > 0 ? Math.round((feedback.good / totalFeedback) * 100) : 0;
+
   return (
-    <div className="appContainer">
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
+    <div>
+      <h1>Sip Happens Café</h1>
+      <p>
+        Please leave your feedback about our service by selecting one of the
+        options below.
+      </p>
+      <Options
+        onLeaveFeedback={updateFeedback}
+        onReset={resetFeedback}
+        totalFeedback={totalFeedback}
       />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
+      {totalFeedback > 0 ? (
+        <Feedback
+          feedback={feedback}
+          totalFeedback={totalFeedback}
+          positivePercentage={positivePercentage}
+        />
+      ) : (
+        <Notification />
+      )}
     </div>
   );
 };
